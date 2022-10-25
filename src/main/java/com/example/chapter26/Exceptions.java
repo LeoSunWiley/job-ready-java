062d5fd09774997a4957727ab968e480e7a7c6de
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-10-25 21:54:20 +0800

    chapter 26

--- src/main/java/com/example/chapter26/Exceptions.java
+++ src/main/java/com/example/chapter26/Exceptions.java
@@ -2,8 +2,13 @@ package com.example.chapter26;
 
 import java.util.Scanner;
 
+/**
+ * Dividing two numbers with some exception handling
+ *
+ * @author leosun
+ */
 public class Exceptions {
-    
+
     public static void main(String[] args) {
         String input;
 
@@ -11,32 +16,32 @@ public class Exceptions {
         int numerator = 0;
         int denominator = 0;
 
-        Scanner myScanner = new Scanner(System.in);
+        try ( Scanner myScanner = new Scanner(System.in)) {
+            try {
+                System.out.println("Please enter the numerator: ");
+                input = myScanner.nextLine();
+                numerator = Integer.parseInt(input);
+            } catch (NumberFormatException ex) {
+                System.out.println("You didn't enter a number for numerator!");
+            }
 
-        System.out.println("Please enter the numerator: ");
-        try {
-            input = myScanner.nextLine();
-            numerator = Integer.parseInt(input);
-        } catch (NumberFormatException ex) {
-            System.out.println("You didn't enter a number for numerator!");
-        }
+            try {
+                System.out.println("Please enter the denominator: ");
+                input = myScanner.nextLine();
+                denominator = Integer.parseInt(input);
+            } catch (NumberFormatException ex) {
+                System.out.println("You didn't enter a number for denominator!");
+            }
 
-        System.out.println("Please enter the denominator: ");
-        try {
-            input = myScanner.nextLine();
-            denominator = Integer.parseInt(input);
-        } catch (NumberFormatException ex) {
-            System.out.println("You didn't enter a number for denominator!");
-        }
-
-        try {
-            quotient = numerator / denominator;
-        } catch (ArithmeticException ex) {
-            System.out.println("Something went wrong: " + ex);
-        }
-
-        System.out.println("The quotient is: " + quotient);
-
-        myScanner.close();
+            try {
+                quotient = numerator / denominator;
+            } catch (ArithmeticException ex) {
+                System.out.println("Something went wrong: " + ex);
+            } finally {
+                System.out.println("finally code block for quotient");
+            }
+            
+            System.out.println("The quotient is: " + quotient);
+        } 
     }
 }
